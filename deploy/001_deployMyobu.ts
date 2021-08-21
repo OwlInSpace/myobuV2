ff04fc8b3c2b7fda93a5c4a397ecce3d2a569f42
Owl In Space <47359751+OwlInSpace@users.noreply.github.com>
2021-08-21 20:08:32 +0300

    Remove useless line

--- deploy/001_deployMyobu.ts
+++ deploy/001_deployMyobu.ts
@@ -1,7 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 import deployContract from "../scripts/deployContract"
-//import { Contract } from "../typechain/Contract"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   await deployContract(
